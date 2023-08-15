6fd4aaa831acfa79235f76a140abf860501c3f18
Nita Constantin <ctinnita88@gmail.com>
2023-08-15 17:33:53 +0300

    lectia 12 - Procedura hard coding

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+function doStartUpConfig() {
+  checkUserLogin();
+  createTable();
+}
+
 function checkUserLogin() {
   const userLoggedIn = sessionStorage.getItem("userLogged");
   console.log(userLoggedIn);
@@ -5,3 +10,87 @@ function checkUserLogin() {
     window.location.replace("./login.html");
   }
 }
+
+let employeesArr = [
+  {
+    name: "Alex",
+    age: 27,
+    project: null,
+    birthDate: "1996-01-10",
+    hired: "2020-10-05",
+    phone: "0722112233",
+    email: "alex@alex.ro",
+  },
+  {
+    name: "Andreea",
+    age: 25,
+    project: null,
+    birthDate: "1998-01-10",
+    hired: "2021-08-21",
+    phone: "07222222",
+    email: "mada@mada.ro",
+  },
+  {
+    name: "Radu",
+    age: 29,
+    project: null,
+    birthDate: "1994-01-10",
+    hired: "2022-10-03",
+    phone: "0722113333",
+    email: "mada@mada.ro",
+  },
+];
+
+function createTable() {
+  const table = document.getElementById("employees_table");
+  let tableString = `<tr>
+  <th>No.</th>
+  <th>Name:</th>
+  <th>Age:</th>
+  <th>Project:</th>
+  <th>Birth Date:</th>
+  <th>Hired at:</th>
+  <th>Phone:</th>
+  <th>Email:</th>
+</tr>`;
+  employeesArr.forEach((employee, i) => {
+    console.log(i);
+    console.log(employee);
+    tableString += createRow(employee, i);
+  });
+  table.innerHTML = tableString;
+  // table.innerHTML = createTableRow();
+}
+
+function createRow(employee, i) {
+  const rowIndex = i + 1;
+  let projectRes = !employee.project ? "-" : employee.project;
+  let rowStr = "<tr>";
+  rowStr += "<td>" + rowIndex + "</td>";
+  rowStr += "<td>" + employee.name + "</td>";
+  rowStr += "<td>" + employee.age + "</td>";
+  rowStr += "<td>" + projectRes + "</td>";
+  rowStr += "<td>" + employee.birthDate + "</td>";
+  rowStr += "<td>" + employee.hired + "</td>";
+  rowStr += "<td>" + employee.phone + "</td>";
+  rowStr += "<td>" + employee.email + "</td>";
+  rowStr += "</tr>";
+  return rowStr;
+}
+
+// function createTableRow() {
+//   let value = "";
+//   employeesArr.forEach((employee, i) => {
+//     i++;
+//     value += `<tr>
+//             <td>${i}</td>
+//             <td>${employee.name}</td>
+//             <td>${employee.age}</td>
+//             <td>${employee.birthDate}</td>
+//             <td>${employee.hired}</td>
+//             <td>${employee.phone}</td>
+//             <td>${employee.email}</td>
+//     </tr>`;
+//   });
+//   return value;
+// }
